1096b8bb6b2e95c907212e0c1563d0f80d68d586
Mari <aura.haze.development@gmail.com>
2023-12-04 03:17:43 -0500

    Can import songs to tracklist now

--- MediaPlayerSDM4/Form1.Designer.cs
+++ MediaPlayerSDM4/Form1.Designer.cs
@@ -29,33 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.musicPlayer = new AxWMPLib.AxWindowsMediaPlayer();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btn_prev = new System.Windows.Forms.Button();
-            this.btn_play = new System.Windows.Forms.Button();
-            this.btn_next = new System.Windows.Forms.Button();
-            this.btn_stop = new System.Windows.Forms.Button();
+            this.track_list = new System.Windows.Forms.ListBox();
             this.btn_open = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).BeginInit();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lbl_msg = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbl_track_start = new System.Windows.Forms.Label();
+            this.lbl_track_end = new System.Windows.Forms.Label();
+            this.musicPlayer = new AxWMPLib.AxWindowsMediaPlayer();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).BeginInit();
             this.SuspendLayout();
-            // 
-            // musicPlayer
-            // 
-            this.musicPlayer.Enabled = true;
-            this.musicPlayer.Location = new System.Drawing.Point(12, 12);
-            this.musicPlayer.Name = "musicPlayer";
-            this.musicPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("musicPlayer.OcxState")));
-            this.musicPlayer.Size = new System.Drawing.Size(782, 338);
-            this.musicPlayer.TabIndex = 0;
-            this.musicPlayer.Visible = false;
-            this.musicPlayer.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter_1);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.track_list);
             this.groupBox1.Controls.Add(this.btn_open);
             this.groupBox1.Controls.Add(this.btn_stop);
             this.groupBox1.Controls.Add(this.btn_next);
@@ -63,7 +59,7 @@
             this.groupBox1.Controls.Add(this.btn_prev);
             this.groupBox1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.groupBox1.Location = new System.Drawing.Point(12, 356);
+            this.groupBox1.Location = new System.Drawing.Point(12, 365);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(782, 184);
             this.groupBox1.TabIndex = 1;
@@ -71,9 +67,66 @@
             this.groupBox1.Text = "CONTROLS";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // track_list
+            // 
+            this.track_list.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.track_list.ForeColor = System.Drawing.Color.Black;
+            this.track_list.FormattingEnabled = true;
+            this.track_list.ItemHeight = 21;
+            this.track_list.Location = new System.Drawing.Point(18, 58);
+            this.track_list.Name = "track_list";
+            this.track_list.Size = new System.Drawing.Size(667, 109);
+            this.track_list.TabIndex = 5;
+            // 
+            // btn_open
+            // 
+            this.btn_open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_open.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_open.Location = new System.Drawing.Point(575, 25);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(110, 27);
+            this.btn_open.TabIndex = 4;
+            this.btn_open.Text = "IMPORT";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_stop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_stop.Location = new System.Drawing.Point(436, 25);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(110, 27);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "STOP";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // btn_next
+            // 
+            this.btn_next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_next.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_next.Location = new System.Drawing.Point(299, 25);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(110, 27);
+            this.btn_next.TabIndex = 2;
+            this.btn_next.Text = "NEXT";
+            this.btn_next.UseVisualStyleBackColor = true;
+            // 
+            // btn_play
+            // 
+            this.btn_play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_play.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_play.Location = new System.Drawing.Point(158, 25);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(110, 27);
+            this.btn_play.TabIndex = 1;
+            this.btn_play.Text = "PLAY";
+            this.btn_play.UseVisualStyleBackColor = true;
+            // 
             // btn_prev
             // 
-            this.btn_prev.ForeColor = System.Drawing.Color.Black;
+            this.btn_prev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_prev.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btn_prev.Location = new System.Drawing.Point(18, 25);
             this.btn_prev.Name = "btn_prev";
             this.btn_prev.Size = new System.Drawing.Size(110, 27);
@@ -81,67 +134,93 @@
             this.btn_prev.Text = "BACK";
             this.btn_prev.UseVisualStyleBackColor = true;
             // 
-            // btn_play
+            // trackBar1
             // 
-            this.btn_play.ForeColor = System.Drawing.Color.Black;
-            this.btn_play.Location = new System.Drawing.Point(152, 25);
-            this.btn_play.Name = "btn_play";
-            this.btn_play.Size = new System.Drawing.Size(110, 27);
-            this.btn_play.TabIndex = 1;
-            this.btn_play.Text = "PLAY";
-            this.btn_play.UseVisualStyleBackColor = true;
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar1.Location = new System.Drawing.Point(704, 25);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 142);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
-            // btn_next
+            // lbl_msg
             // 
-            this.btn_next.ForeColor = System.Drawing.Color.Black;
-            this.btn_next.Location = new System.Drawing.Point(282, 25);
-            this.btn_next.Name = "btn_next";
-            this.btn_next.Size = new System.Drawing.Size(110, 27);
-            this.btn_next.TabIndex = 2;
-            this.btn_next.Text = "NEXT";
-            this.btn_next.UseVisualStyleBackColor = true;
+            this.lbl_msg.AutoSize = true;
+            this.lbl_msg.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_msg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_msg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbl_msg.Location = new System.Drawing.Point(12, 5);
+            this.lbl_msg.Name = "lbl_msg";
+            this.lbl_msg.Size = new System.Drawing.Size(80, 13);
+            this.lbl_msg.TabIndex = 2;
+            this.lbl_msg.Text = "Media Player";
+            this.lbl_msg.Click += new System.EventHandler(this.label1_Click);
             // 
-            // btn_stop
+            // progressBar1
             // 
-            this.btn_stop.ForeColor = System.Drawing.Color.Black;
-            this.btn_stop.Location = new System.Drawing.Point(416, 25);
-            this.btn_stop.Name = "btn_stop";
-            this.btn_stop.Size = new System.Drawing.Size(110, 27);
-            this.btn_stop.TabIndex = 3;
-            this.btn_stop.Text = "STOP";
-            this.btn_stop.UseVisualStyleBackColor = true;
+            this.progressBar1.Location = new System.Drawing.Point(57, 352);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(685, 10);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
-            // btn_open
+            // lbl_track_start
             // 
-            this.btn_open.ForeColor = System.Drawing.Color.Black;
-            this.btn_open.Location = new System.Drawing.Point(547, 25);
-            this.btn_open.Name = "btn_open";
-            this.btn_open.Size = new System.Drawing.Size(110, 27);
-            this.btn_open.TabIndex = 4;
-            this.btn_open.Text = "IMPORT";
-            this.btn_open.UseVisualStyleBackColor = true;
+            this.lbl_track_start.AutoSize = true;
+            this.lbl_track_start.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_track_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_track_start.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbl_track_start.Location = new System.Drawing.Point(12, 349);
+            this.lbl_track_start.Name = "lbl_track_start";
+            this.lbl_track_start.Size = new System.Drawing.Size(39, 13);
+            this.lbl_track_start.TabIndex = 4;
+            this.lbl_track_start.Text = "00:00";
             // 
-            // listBox1
+            // lbl_track_end
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(18, 58);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(639, 109);
-            this.listBox1.TabIndex = 5;
+            this.lbl_track_end.AutoSize = true;
+            this.lbl_track_end.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_track_end.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_track_end.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbl_track_end.Location = new System.Drawing.Point(748, 352);
+            this.lbl_track_end.Name = "lbl_track_end";
+            this.lbl_track_end.Size = new System.Drawing.Size(39, 13);
+            this.lbl_track_end.TabIndex = 5;
+            this.lbl_track_end.Text = "00:00";
+            // 
+            // musicPlayer
+            // 
+            this.musicPlayer.Enabled = true;
+            this.musicPlayer.Location = new System.Drawing.Point(15, 21);
+            this.musicPlayer.Name = "musicPlayer";
+            this.musicPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("musicPlayer.OcxState")));
+            this.musicPlayer.Size = new System.Drawing.Size(782, 322);
+            this.musicPlayer.TabIndex = 0;
+            this.musicPlayer.Visible = false;
+            this.musicPlayer.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(809, 552);
+            this.Controls.Add(this.lbl_track_end);
+            this.Controls.Add(this.lbl_track_start);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lbl_msg);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.musicPlayer);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musicPlayer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -152,8 +231,13 @@
         private System.Windows.Forms.Button btn_next;
         private System.Windows.Forms.Button btn_play;
         private System.Windows.Forms.Button btn_prev;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox track_list;
         private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lbl_msg;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lbl_track_start;
+        private System.Windows.Forms.Label lbl_track_end;
     }
 }
 
--- MediaPlayerSDM4/Form1.cs
+++ MediaPlayerSDM4/Form1.cs
@@ -26,5 +26,37 @@ namespace MediaPlayerSDM4
         {
 
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string[] paths, files;
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+                for(int x=0;x<files.Length;x++)
+                {
+                    track_list.Items.Add(files[x]);
+                }
+            }
+        }
     }
 }
